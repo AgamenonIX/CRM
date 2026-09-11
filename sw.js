4adf6271966ef617cd2ee0736427e851980d8bf5
AgamenonIX <ezaquiel.9@gmail.com>
2026-09-11 10:13:34 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'ocorrencia-rac-v1';
+const ARQUIVOS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARQUIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((chaves) =>
+      Promise.all(chaves.filter((c) => c !== CACHE_NAME).map((c) => caches.delete(c)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resposta) => resposta || fetch(event.request))
+  );
+});
